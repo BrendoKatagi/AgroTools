2d6e83aa90c93538e1e835d762ff25cf1fd34ec8
Brendo Katagi <brendo.katagi@gmail.com>
2021-03-30 20:01:58 -0300

    feat: create json with objects from new form

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejs = require("ejs");
 
 const app = express();
 let forms = [];
+let qId = 1;
+let createItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +15,7 @@ app.use(express.static("public"));
 app.route("/")
 
 .get(function(req, res){
-    res.render("index");
+    res.render("index", {questId: qId});
 })
 
 .post(function(req, res){
@@ -29,6 +31,36 @@ app.route("/")
     forms.push(newForm);
 
     console.log(forms);
+});
+
+app.route("/create")
+.get(function(req, res){
+    res.render("create", {questId: qId + 1, questions: createItems});
+})
+
+.post(function(req, res){
+    console.log(req.body);
+    let newValue = req.body.newItem;
+    
+    if(req.body.button === 'add'){
+        createItems.push(newValue);
+        console.log(createItems);
+        res.render("create", {questId: qId +1 , questions: createItems});
+
+    }else if(req.body.button === 'save'){
+        qId += 1;
+
+        const newForm = {formIndex: qId};
+        JSON.stringify(newForm);
+        JSON.stringify(createItems);
+        newForm.items = createItems;
+
+        forms.push(newForm);
+        console.log(forms);
+        createItems = [];
+   
+    }
+    //console.log(req.body);
 });
 
 app.listen(3000, function(){
