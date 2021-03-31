fbb1e425b87819eab6bd3a4529c3b627b0effe9a
Brendo Katagi <brendo.katagi@gmail.com>
2021-03-31 00:47:08 -0300

    feat: create route to modal page

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -7,6 +6,7 @@ const app = express();
 let forms = [];
 let qId = 1;
 let createItems = [];
+let newForm = {};
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -33,6 +33,25 @@ app.route("/")
     console.log(forms);
 });
 
+app.route("/modal")
+.get(function(req, res){
+    res.render("modal");
+})
+
+.post(function(req, res){
+
+    newForm = {
+        formIndex: qId + 1,
+        creatorName: req.body.name,
+        title: req.body.formTitle,
+        country: req.body.country,
+        city: req.body.city
+    };
+    JSON.stringify(newForm);
+    res.redirect("/create");
+    
+});
+
 app.route("/create")
 .get(function(req, res){
     res.render("create", {questId: qId + 1, questions: createItems});
@@ -43,6 +62,7 @@ app.route("/create")
     let newValue = req.body.newItem;
     
     if(req.body.button === 'add'){
+
         createItems.push(newValue);
         console.log(createItems);
         res.render("create", {questId: qId +1 , questions: createItems});
@@ -50,18 +70,19 @@ app.route("/create")
     }else if(req.body.button === 'save'){
         qId += 1;
 
-        const newForm = {formIndex: qId};
-        JSON.stringify(newForm);
         JSON.stringify(createItems);
         newForm.items = createItems;
 
         forms.push(newForm);
         console.log(forms);
         createItems = [];
-   
+        newForm = {};
     }
     //console.log(req.body);
 });
+
+app.route("/answer")
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
